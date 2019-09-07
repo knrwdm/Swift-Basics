ba0e16b20e97bfcf63f22c8da538516b063ab555
Kayla <knrwdm@mail.missouri.edu>
2019-09-06 21:05:50 -0500

    First commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Kayla Reap on 9/6/19.
+//  Copyright © 2019 Kayla Reap. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
